03d65d9a0fc692ea1f0e4f5dec853ed897ccebaa
mchurch <mchurc90@uga.edu>
2014-12-15 14:21:15 -0500

    Finished up most of the work on the android app. Just need to integrate with other parts of the system. Also am trying to get the app to run in the background a little better.

---
+++ SitiAtMobile/src/com/example/sitiatmobile/Preferences.java
@@ -0,0 +1,19 @@
+package com.example.sitiatmobile;
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+
+public class Preferences extends PreferenceFragment {
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.preferences);
+	}
+	
+		
+
+}
+
+
